0374e7ec7f64dbb8db6eac0b0aa3dc3aabaf9905
SimonRennocks <rennocks@live.com>
2017-09-12 15:13:15 -0400

    k

--- winFunc.py
+++ winFunc.py
@@ -2,18 +2,26 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
+# Windows
 s = pd.Series(np.random.randn(1000), index=pd.date_range('1/1/2000', periods=1000))
 s = s.cumsum()
 r = s.rolling(window=60)
 
-s.plot(style='k--')
-r.mean().plot(style='k')
+#s.plot(style='k--')
+#r.mean().plot(style='k')
 
+# Expanding Windows
+s.plot(style='k--')
+s.ewm(span=20).mean().plot(style='k')
+
+#data frames
 df = pd.DataFrame(np.random.randn(1000, 4),
     index=pd.date_range('1/1/2000', periods=1000),
     columns=['A', 'B', 'C', 'D'])
 
 df = df.cumsum()
 df.rolling(window=60).sum().plot(subplots=True)
+
+
 
 plt.show()
